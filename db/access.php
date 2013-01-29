60d96c438787671dbf4271a76b8ff930ce080ebc
Dietmar Angerer <dangerer@gtn-solutions.com>
2013-01-29 15:50:37 +0100

    new capability addinstance

--- db/access.php
+++ db/access.php
@@ -49,6 +49,14 @@
 defined('MOODLE_INTERNAL') || die();
 
 $capabilities = array(
-
+	'mod/exagames:addinstance' => array(
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+        'clonepermissionsfrom' => 'moodle/course:manageactivities'
+    ),
 );
 
